2ff5417886f87058bac076afe65b578e7ff71151
ArindamGhoshAni <arindamghosh010@gmail.com>
2022-07-22 14:23:14 +0530

    final test

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 day 2
 try to 
+
+
+eddcf86d-9e5c-4b4c-a6f6-66d88abd20b3
+
+testappani
--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 
 def index():
-    return "MY APP"
+    return "I completed this deployment"
 
 if __name__=="__main__":
     app.run(debug=True)
