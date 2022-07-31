d2fa91c4d321675ba4fced9f6a7f3b2ce3cf3e93
ArindamGhoshAni <arindamghosh010@gmail.com>
2022-07-31 21:50:31 +0530

    app

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-day 2
-try to 
 
 
-eddcf86d-9e5c-4b4c-a6f6-66d88abd20b3
 
-testappani
--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask  import   Flask , render_template, request
+import numpy as np
+import jsonify
+import pickle
+
+
 
 app = Flask(__name__)
 
-@app.route("/",methods=['GET','POST'])
+model = pickle.load(open('insurance_predict_model.pkl', 'rb'))
 
-def index():
-    return "I completed this deployment"
+@app.route('/',methods=['GET'])
+def home():
+    return render_template('home.html')
 
-if __name__=="__main__":
+@app.route('/index',methods=['GET'])
+def hello():
+    return render_template('index.html')
+    
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        age = int(request.form['a'])
+        sex = request.form['sex']
+        bmi = float(request.form['c'])
+        child = int(request.form['d'])
+        smoker = request.form['smoker']
+
+        region = request.form['region']
+        prediction = model.predict([[age , sex, bmi, child, smoker, region]])
+        output=round(prediction[0],2)
+        return render_template('index.html',prediction_text="Your predicted premium is : {}".format(output))
+    else:
+        return render_template('index.html')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
